6ffb857289ad6a0de85317cf519c088203a36dee
SuraYutKets <sasalak.m@rmutsvmail.com>
2026-02-20 00:45:13 +0700

    fix setup

--- setup.php
+++ setup.php
@@ -1,11 +1,12 @@
 <?php
 try {
-	$db = new PDO("sqlite:/tmp/database.db");
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = file_get_contents("init.sql");
-    	$db->exec($sql);
-    echo "Database initialized successfully!";
+    $db = new PDO("sqlite:/tmp/database.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = file_get_contents(__DIR__ . "/init.sql");
+    $db->exec($sql);
+
 } catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
+    echo "Setup Error: " . $e->getMessage();
 }
 ?>
