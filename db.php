a366b03676d9d847f58e00885ffb7e39cae22e9f
SuraYutKets <sasalak.m@rmutsvmail.com>
2026-02-20 00:38:56 +0700

    fix sqlite path for render

--- db.php
+++ db.php
@@ -1,6 +1,12 @@
 <?php
 $dbPath = "/tmp/database.db";
 
+$firstTime = !file_exists($dbPath);
+
 $conn = new PDO("sqlite:" . $dbPath);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($firstTime) {
+    require_once "setup.php";
+}
 ?>
